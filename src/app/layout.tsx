217f034ece61f73d8c7135e5eea6786b8f49bfd1
Jose Nunez <jcnv21@gmail.com>
2024-03-06 03:24:45 -0600

    remove more files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,13 @@
-import Navbar from "@/components/Navbar";
 import "./globals.css";
-
-import Providers from "@/components/template/layout/providers";
-import { Toaster } from "@/components/ui/toaster";
-import { getServerSession } from "next-auth";
 
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await getServerSession();
-
   return (
     <html lang="en" suppressHydrationWarning>
-      {/* className="overflow-hidden" */}
-      <body>
-        <Providers session={session}>
-          <Toaster />
-          {children}
-        </Providers>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
